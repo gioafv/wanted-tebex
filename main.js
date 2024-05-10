6c373cc2bfe7af25be8074d2c004e340a69c77d8
Giovane Silva <contatogiovanes@gmail.com>
2024-05-10 00:58:14 -0300

    build: header finalizado

---
+++ main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById('topnav');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+
+    var x = document.getElementById('myLinks');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
